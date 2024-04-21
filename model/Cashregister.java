9fa0c5ddef9ca503e5623934db9fdf78b258846d
03Elias <brofist.man43@gmail.com>
2024-04-21 17:23:58 +0200

    Updated the Cashregister class by implementing the logic for the different methods. Not finshed but a rough draft.

--- model/Cashregister.java
+++ model/Cashregister.java
@@ -10,9 +10,19 @@ public class Cashregister {
      * The method updates the cash in the register after a sale.
      * 
      * @param amountPaid The amount that the customer paid for the sale.
+     * @param saleInof   The sale information that is needed for updating the cash
+     *                   in the register espacially the total price info.
      */
-    public void updateCashInRegister(double amountPaid) {
+    public void updateCashInRegister(double amountPaid, SaleDTO saleInfo) {
+        double totalPrice = saleInfo.getTotalPrice();
+        double cashToBePutInToRegister = totalPrice - amountPaid;
+        if (cashToBePutInToRegister > 0) {
+            cashToBePutInToRegister = 0; // fix more, the tought is if person pay less than
+                                         // totalPrice then no cash will be over and person needs to pay the rest.
+        } else if (cashToBePutInToRegister <= 0) {
+            cashInRegister += cashToBePutInToRegister;
 
+        }
     }
 
     /**
@@ -29,12 +39,19 @@ public class Cashregister {
      * Calculates the change that the customer is given after a sale.
      * 
      * @param amountPaid The amount that the customer paid.
-     * @param saleInfo   The sale information specifically the total price.
+     * @param saleInfo   The sale information specifically the total price is the
+     *                   desired part.
      * @return The calculated change that the customer is given.
      */
     public double calculatedChange(double amountPaid, SaleDTO saleInfo) {
-        return change; // Incorrect
 
+        double totalPrice = saleInfo.getTotalPrice();
+        change = amountPaid - totalPrice;
+        if (change > 0) {
+            return change;
+        } else {
+            return 0;
+        }
     }
 
 }
