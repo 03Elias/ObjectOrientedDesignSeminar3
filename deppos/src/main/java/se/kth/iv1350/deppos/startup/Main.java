0d772137f9d5ebb6b23354b159b172d499fe7460
danljungstrom <danljun@kth.se>
2024-04-24 17:25:48 +0200

    Updated main packages

--- deppos/src/main/java/se/kth/iv1350/deppos/startup/Main.java
+++ deppos/src/main/java/se/kth/iv1350/deppos/startup/Main.java
@@ -1,12 +1,12 @@
-package startup;
+package se.kth.iv1350.deppos.startup;
 
-import controller.Controller;
-import integration.DiscountHandler;
-import integration.ExternalAccountSystemHandler;
-import integration.SalelogHandler;
-import integration.ExternalInventorySystemHandler;
-import model.Cashregister;
-import view.View;
+import se.kth.iv1350.deppos.controller.Controller;
+import se.kth.iv1350.deppos.integration.DiscountHandler;
+import se.kth.iv1350.deppos.integration.ExternalAccountSystemHandler;
+import se.kth.iv1350.deppos.integration.SalelogHandler;
+import se.kth.iv1350.deppos.integration.ExternalInventorySystemHandler;
+import se.kth.iv1350.deppos.model.CashRegister;
+import se.kth.iv1350.deppos.view.View;
 
 public class Main {
 
@@ -20,7 +20,7 @@ public class Main {
         SalelogHandler slh = new SalelogHandler();
         ExternalAccountSystemHandler eash = new ExternalAccountSystemHandler();
         ExternalInventorySystemHandler eish = new ExternalInventorySystemHandler();
-        Cashregister cr = new Cashregister();   //Fanns inte med i SSD:n
+        CashRegister cr = new CashRegister();   //Fanns inte med i SSD:n
 
         Controller controller = new Controller(eish, eash, dh, slh, cr);
 
