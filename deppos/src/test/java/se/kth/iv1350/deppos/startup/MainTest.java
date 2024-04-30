8700f64e9022ac4ca73852bd3ee8b92d9f1c7213
danljungstrom <danljun@kth.se>
2024-04-30 22:53:31 +0200

    Improved testnames and made minor adjustments, final sem 3

--- deppos/src/test/java/se/kth/iv1350/deppos/startup/MainTest.java
+++ deppos/src/test/java/se/kth/iv1350/deppos/startup/MainTest.java
@@ -1,17 +1,12 @@
 package se.kth.iv1350.deppos.startup;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
 
-    @BeforeEach
-    public void setup() {
-        //Placeholder
-    }
-
     @Test
     public void testMain() {
-        //Placeholder test
+        assertDoesNotThrow(() -> Main.main(null));
     }
 }
