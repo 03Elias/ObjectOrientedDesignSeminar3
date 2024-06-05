3e8698917f6b211006c205a39d4e7af30ba44a3e
03Elias <brofist.man43@gmail.com>
2024-06-05 14:40:56 +0200

    Javadoc comments added

--- deppos/src/main/java/se/kth/iv1350/deppos/model/RevenueObserver.java
+++ deppos/src/main/java/se/kth/iv1350/deppos/model/RevenueObserver.java
@@ -1,15 +1,21 @@
 package se.kth.iv1350.deppos.model;
 
 public abstract class RevenueObserver {
-    // This is the method defined in the observer interface.
+
+    /**
+     * Adds the price of the sale to the total income.
+     * @param priceOfTheSaleThatWasJustMade The price of the sale that was just made.
+     */
     public void update(double priceOfTheSaleThatWasJustMade) {
-        calculateTotalIncome(priceOfTheSaleThatWasJustMade); // Calculate the total amount paid since the program
-                                                             // started.
+        calculateTotalIncome(priceOfTheSaleThatWasJustMade); 
         showTotalIncome();
     }
 
     protected abstract void calculateTotalIncome(double priceOfTheSaleThatWasJustMade);
 
+    /**
+     * Prints the total amount of money earned since the program started.
+     */
     private void showTotalIncome() {
         try {
             doShowTotalIncome();
